fc844a514c193a9015452b6ae1f3a9b14bea34ef
Chandler Calkins <chandlerjaycalkins@gmail.com>
2023-04-18 18:15:52 -0700

    Updated raspberry pi end effector control program

--- Raspberry_Pi/end_effector_control.py
+++ Raspberry_Pi/end_effector_control.py
@@ -4,135 +4,241 @@ import time
 import RPi.GPIO as gpio
 
 # function for waiting for a tcp message from a socket
-def getMessage(conn):
+def getMsg(conn):
 	msg = ""
 	while True:
 		msg = conn.recv(4)
 		if len(msg) != 0:
 			return msg
 
-# define ip address and port number of raspberry pi servers in a tuple
-host = "192.168.0.100"
-johnny5_port = 5001
-alpha_port = 5002
-johnny5_server_addr = (host, johnny5_port)
-alpha_server_addr = (host, alpha_port)
+# prints a message received from one of the robotic arms
+def printMsg(name, addr, msg):
+	print(f"Message from {name} on {addr[0]}:{addr[1]}:")
+	print(msg)
 
-# use board numbers for raspberry pi gpio
-gpio.setmode(gpio.BOARD)
+while True:
+	try:
+		############################################################
+		#
+		# Set Up
+		#
+		############################################################
 
-# define spray end effector pins
-pin_spray_power = 11 # gpio 17
-pin_spray_valve = 13 # gpio 27
+		# define ip address and port number of raspberry pi servers in a tuple
+		host = "192.168.0.100"
+		johnny5_port = 5001
+		alpha_port = 5002
+		johnny5_server_addr = (host, johnny5_port)
+		alpha_server_addr = (host, alpha_port)
 
-# define vacuum gripper / suction cups pin
-pin_succ = 22 # gpio 25
+		# use board numbers for raspberry pi gpio
+		gpio.setmode(gpio.BOARD)
 
-# define conductive ink end effector pins
-pin_ink_driver = 15 # gpio 22
-pin_ink_forward = 16 # gpio 23
-pin_ink_reverse = 18 # gpio 24
+		# define spray end effector pins
+		pin_spray_power = 11 # gpio 17
+		pin_spray_valve = 13 # gpio 27
 
-# set spray end effector pins to outputs
-gpio.setup(pin_spray_power, gpio.OUT)
-gpio.setup(pin_spray_valve, gpio.OUT)
+		# define vacuum gripper / suction cups pin
+		pin_succ = 22 # gpio 25
 
-# set vacuum gripper / suction cups pin to output
-gpio.setup(pin_succ, gpio.OUT)
+		# define conductive ink end effector pins
+		pin_ink_driver = 15 # gpio 22
+		pin_ink_forward = 16 # gpio 23
+		pin_ink_reverse = 18 # gpio 24
 
-# set conductive ink end effector pins to outputs
-gpio.setup(pin_ink_driver, gpio.OUT)
-gpio.setup(pin_ink_forward, gpio.OUT)
-gpio.setup(pin_ink_reverse, gpio.OUT)
+		# set spray end effector pins to outputs
+		gpio.setup(pin_spray_power, gpio.OUT)
+		gpio.setup(pin_spray_valve, gpio.OUT)
 
-# define sockets to connect to robot arm controllers
-johnny5_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-alpha_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		# set vacuum gripper / suction cups pin to output
+		gpio.setup(pin_succ, gpio.OUT)
 
-# bind johnny 5 socket to ip address and port
-try:
-	johnny5_sock.bind(johnny5_server_addr)
-except:
-	#print(f"Bind failed. Error code: {message[0]} Message {message[1]}")
-	print("Bind failed on johnny5_server_addr")
-	gpio.cleanup()
-	sys.exit()
+		# set conductive ink end effector pins to outputs
+		gpio.setup(pin_ink_driver, gpio.OUT)
+		gpio.setup(pin_ink_forward, gpio.OUT)
+		gpio.setup(pin_ink_reverse, gpio.OUT)
 
-# bind alpha socket to ip address and port
-try:
-	alpha_sock.bind(alpha_server_addr)
-except:
-	#print(f"Bind failed. Error code: {message[0]} Message {message[1]}")
-	print("Bind failed on alpha_server_addr")
-	gpio.cleanup()
-	sys.exit()
+		# define sockets to connect to robot arm controllers
+		johnny5_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		alpha_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-print("Socket binding operation completed")
-msg = ""
+		# bind johnny 5 socket to ip address and port
+		try:
+			johnny5_sock.bind(johnny5_server_addr)
+		except:
+			#print(f"Bind failed. Error code: {message[0]} Message {message[1]}")
+			print("Bind failed on johnny5_server_addr")
+			gpio.cleanup()
+			sys.exit()
 
-# establish connection with johnny 5
-johnny5_sock.listen(9)
-print(f"Listening for Johnny 5 on {host}:{johnny5_port}...")
-johnny5_conn, johnny5_addr = johnny5_sock.accept()
-print(f"Connected with Johnny 5 on {johnny5_addr[0]}:{johnny5_addr[1]}")
+		# bind alpha socket to ip address and port
+		try:
+			alpha_sock.bind(alpha_server_addr)
+		except:
+			#print(f"Bind failed. Error code: {message[0]} Message {message[1]}")
+			print("Bind failed on alpha_server_addr")
+			gpio.cleanup()
+			sys.exit()
 
-# get opening message from johnny 5
-#while True:
-#	msg = johnny5_conn.recv(4).decode("utf-8")
-#	if len(msg) != 0:
-#		break
-msg = getMessage(johnny5_conn).decode("utf-8")
+		print("Socket binding operation completed")
+		msg = ""
 
-# print message from johnny 5
-print(f"Message from Johnny 5 on {johnny5_addr[0]}:{johnny5_addr[1]}:")
-print(msg)
+		############################################################
+		#
+		# Connect to Robot Arms
+		#
+		############################################################
 
-# establish connection with alpha
-alpha_sock.listen(9)
-print(f"Listening for Alpha on {host}:{alpha_port}...")
-alpha_conn, alpha_addr = alpha_sock.accept()
-print(f"Connected with Alpha on {alpha_addr[0]}:{alpha_addr[1]}")
+		# establish connection with johnny 5
+		johnny5_sock.listen(9)
+		print(f"Listening for Johnny 5 on {host}:{johnny5_port}...")
+		johnny5_conn, johnny5_addr = johnny5_sock.accept()
+		print(f"Connected with Johnny 5 on {johnny5_addr[0]}:{johnny5_addr[1]}")
 
-# get opening message from alpha
-#while True:
-#	msg = alpha_conn.recv(4).decode("utf-8")
-#	if len(msg) != 0:
-#		break
-msg = getMessage(alpha_conn).decode("utf-8")
+		# get opening message from johnny 5
+		msg = getMsg(johnny5_conn).decode("utf-8")
+		# print message from johnny 5
+		printMsg("Johnny 5", johnny5_addr, msg)
 
-# print message from alpha
-print(f"Message from Alpha on {alpha_addr[0]}:{alpha_addr[1]}:")
-print(msg)
+		# establish connection with alpha
+		alpha_sock.listen(9)
+		print(f"Listening for Alpha on {host}:{alpha_port}...")
+		alpha_conn, alpha_addr = alpha_sock.accept()
+		print(f"Connected with Alpha on {alpha_addr[0]}:{alpha_addr[1]}")
 
-# get message from johnny 5
-#while True:
-#	msg = johnny5_conn.recv(4).decode("utf-8")
-#	if len(msg) != 0:
-#		break
-msg = getMessage(johnny5_conn).decode("utf-8")
+		# get opening message from alpha
+		msg = getMsg(alpha_conn).decode("utf-8")
+		# print message from alpha
+		printMsg("Alpha", alpha_addr, msg)
 
-# print message from johnny 5
-print(f"Message from Johnny 5 on {johnny5_addr[0]}:{johnny5_addr[1]}:")
-print(msg)
+		############################################################
+		#
+		# Step 1: Spray Adhesive
+		#
+		############################################################
 
-# activate spray
-gpio.output(pin_spray_power, gpio.HIGH)
-gpio.output(pin_spray_valve, gpio.HIGH)
-print("Activated sprayer...")
+		# get message from johnny 5
+		msg = getMsg(johnny5_conn).decode("utf-8")
+		# print message from johnny 5
+		printMsg("Johnny 5", johnny5_addr, msg)
 
-# get message from johnny 5
-msg = getMessage(johnny5_conn).decode("utf-8")
+		# activate spray
+		gpio.output(pin_spray_power, gpio.HIGH)
+		gpio.output(pin_spray_valve, gpio.HIGH)
+		print("Activated sprayer...")
 
-# print message from johnny 5
-print(f"Message from Johnny 5 on {johnny5_addr[0]}:{johnny5_addr[1]}:")
-print(msg)
+		# get message from johnny 5
+		msg = getMsg(johnny5_conn).decode("utf-8")
+		# print message from johnny 5
+		printMsg("Johnny 5", johnny5_addr, msg)
 
-# deactivate spray
-gpio.output(pin_spray_valve, gpio.LOW)
-gpio.output(pin_spray_power, gpio.LOW)
-print("Deactivated sprayer...")
+		# deactivate spray
+		gpio.output(pin_spray_valve, gpio.LOW)
+		gpio.output(pin_spray_power, gpio.LOW)
+		print("Deactivated sprayer...")
 
-# reset gpio board and close tcp sessions
-gpio.cleanup()
-johnny5_conn.close()
-alpha_conn.close()
+		############################################################
+		#
+		# Step 2: Place Solar Cells
+		#
+		############################################################
+
+		# pick up and place 4 sets of solar cells
+		for i in range(4):
+			# get message from alpha
+			msg = getMsg(alpha_conn).decode("utf-8")
+			# print message from alpha
+			printMsg("Alpha", alpha_addr, msg)
+
+			# activate suction cups
+			gpio.output(pin_succ, gpio.HIGH)
+			print("Activated suction cups...")
+
+			# get message from alpha
+			msg = getMsg(alpha_conn).decode("utf-8")
+			# print message from alpha
+			printMsg("Alpha", alpha_addr, msg)
+
+			# activate suction cups
+			gpio.output(pin_succ, gpio.LOW)
+			print("Deactivated suction cups...")
+
+		############################################################
+		#
+		# Step 3: Apply Conductive Ink
+		#
+		############################################################
+
+		# forward and reverse 23 times
+		for i in range(23):
+			# get message from johnny 5
+			msg = getMsg(johnny5_conn).decode("utf-8")
+			# print message from johnny 5
+			printMsg("Johnny 5", johnny5_addr, msg)
+
+			# activate ink applier driver and make it go forward
+			gpio.output(pin_ink_driver, gpio.HIGH)
+			gpio.output(pin_ink_forward, gpio.HIGH)
+			print("Making ink applier go forward...")
+
+			# get message from johnny 5
+			msg = getMsg(johnny5_conn).decode("utf-8")
+			# print message from johnny 5
+			printMsg("Johnny 5", johnny5_addr, msg)
+
+			# make ink applier go in reverse
+			gpio.output(pin_ink_forward, gpio.LOW)
+			gpio.output(pin_ink_reverse, gpio.HIGH)
+			print("Making ink applier go in reverse...")
+
+			# get message from johnny 5
+			msg = getMsg(johnny5_conn).decode("utf-8")
+			# print message from johnny 5
+			printMsg("Johnny 5", johnny5_addr, msg)
+
+			# turn off ink applier
+			gpio.output(pin_ink_reverse, gpio.LOW)
+			gpio.output(pin_ink_driver, gpio.LOW)
+			print("Turning off ink applier...")
+
+		############################################################
+		#
+		# Step 4: Spray Protective Coating
+		#
+		############################################################
+
+		# get message from johnny 5
+		msg = getMsg(johnny5_conn).decode("utf-8")
+		# print message from johnny 5
+		printMsg("Johnny 5", johnny5_addr, msg)
+
+		# activate spray
+		gpio.output(pin_spray_power, gpio.HIGH)
+		gpio.output(pin_spray_valve, gpio.HIGH)
+		print("Activated sprayer...")
+
+		# get message from johnny 5
+		msg = getMsg(johnny5_conn).decode("utf-8")
+		# print message from johnny 5
+		printMsg("Johnny 5", johnny5_addr, msg)
+
+		# deactivate spray
+		gpio.output(pin_spray_valve, gpio.LOW)
+		gpio.output(pin_spray_power, gpio.LOW)
+		print("Deactivated sprayer...")
+
+		############################################################
+		#
+		# Finish
+		#
+		############################################################
+
+		# reset gpio board and close tcp sessions
+		gpio.cleanup()
+		johnny5_conn.close()
+		alpha_conn.close()
+	except:
+		# reset gpio board and close tcp sessions
+		gpio.cleanup()
+		johnny5_conn.close()
+		alpha_conn.close()
