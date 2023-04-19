ef27b7477a0f02ec4bbb62af855fa08d33bc226e
Chandler Calkins <chandlerjaycalkins@gmail.com>
2023-04-19 11:53:43 -0700

    Made an improved gpio test program

--- Raspberry_Pi/gpio_test.py
+++ Raspberry_Pi/gpio_test.py
@@ -1,19 +1,35 @@
 import RPi.GPIO as gpio
 
 gpio.setmode(gpio.BOARD)
+pins = []
 
-pin = 36
+#pin = 36
 
-gpio.setup(pin, gpio.OUT)
+#gpio.setup(pin, gpio.OUT)
 
-while True:
-	ch = input ("Press enter to toggle GPIO or q to quit: ")
-	if ch.lower() == "q":
-		break
-	gpio.output(pin, gpio.HIGH)
-	ch = input("Press enter to toggle GPIO or q to quit: ")
-	if ch.lower() == "q":
-		break
-	gpio.output(pin, gpio.LOW)
+try:
+	while True:
+		#ch = input ("Press enter to toggle GPIO or q to quit: ")
+		#if ch.lower() == "q":
+		#	break
+		#gpio.output(pin, gpio.HIGH)
+		#ch = input("Press enter to toggle GPIO or q to quit: ")
+		#if ch.lower() == "q":
+		#	break
+		#gpio.output(pin, gpio.LOW)
+		pin = input("Enter pin number to set: ")
+		pin = int(pin)
+		if pin not in pins:
+			pins.append(pin)
+			gpio.setup(pin, gpio.OUT)
+		out = input("Enter on or off: ")
+		if out.lower() == "on":
+			gpio.output(pin, gpio.HIGH)
+		else:
+			gpio.output(pin, gpio.LOW)
+except:
+	print("\nExiting program...")
+	for pin in pins:
+		gpio.output(pin, gpio.LOW)
 
 gpio.cleanup()
